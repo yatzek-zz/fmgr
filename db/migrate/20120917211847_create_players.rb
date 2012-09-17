37922d078f16ea44c6edb72b4acaf51b83b2baa4
Jacek Szlachta <jacek.szlachta@gmail.com>
2012-09-17 23:20:32 +0100

    players scaffold, index page

---
+++ db/migrate/20120917211847_create_players.rb
@@ -0,0 +1,12 @@
+class CreatePlayers < ActiveRecord::Migration
+  def change
+    create_table :players do |t|
+      t.primary_key :id
+      t.string :name
+      t.string :surname
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
