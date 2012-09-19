c8b3d7ef7c2bef4265c2afa3203373dc1ed0193d
Jacek Szlachta <jacek.szlachta@gmail.com>
2012-09-19 22:34:53 +0100

    player id encryption

--- app/helpers/players_helper.rb
+++ app/helpers/players_helper.rb
@@ -1,2 +1,15 @@
+require "base64"
+require "encryptor"
+Encryptor.default_options.merge!(:key => Digest::SHA256.hexdigest('very secret'))
+
 module PlayersHelper
+
+  def url_safe_encode value
+    Base64.urlsafe_encode64(value.encrypt)
+  end
+
+  def url_safe_decode value
+    Base64.urlsafe_decode64(value).decrypt
+  end
+
 end
