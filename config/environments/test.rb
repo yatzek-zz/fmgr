248a5cb596139b3e091a353fb6f07799cb8ee701
Jacek Szlachta <jacek.szlachta@gmail.com>
2012-11-18 21:53:03 +0000

    JS: non-blocking postgres

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,8 @@
 Tfmgr::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.middleware.insert_before ActionDispatch::Static, Rack::FiberPool, :size => 100
+
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
@@ -34,6 +36,4 @@ Tfmgr::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
-
-  config.middleware.insert_before ActionDispatch::Static, Rack::FiberPool, :size => 100
 end
