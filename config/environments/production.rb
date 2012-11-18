e12db444bf8046191ee46b323c80bbb0a842aabf
Jacek Szlachta <jacek.szlachta@gmail.com>
2012-11-18 21:47:11 +0000

    JS: non-blocking postgres

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,6 @@ Tfmgr::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.middleware.insert_before Rack::Lock, Rack::FiberPool, :size => 100
+  config.middleware.insert_before "Rack::Lock", Rack::FiberPool, :size => 100
 
 end
