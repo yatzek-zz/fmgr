39ab7da5f6e87b1cb7b3d396a6b9f5aa8c07254b
Jacek Szlachta <jacek.szlachta@gmail.com>
2012-11-18 21:59:38 +0000

    JS: non-blocking postgres

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Tfmgr::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
-  config.middleware.use Rack::FiberPool, :size => 100
+  config.middleware.insert_before ActionDispatch::Static, Rack::FiberPool, :size => 100
 
   # Code is not reloaded between requests
   config.cache_classes = true
@@ -11,7 +11,7 @@ Tfmgr::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  #config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
