f2ce1c5713e6e78a38eb6549d6ba3dd74b86d102
Jacek Szlachta <jacek.szlachta@gmail.com>
2013-01-12 17:57:12 +0000

    fix issue with rake not loading dependencies in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,7 @@ Tfmgr::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  # rails classes not loaded when running rake scheduler tasks in prod
+  config.dependency_loading = true if $rails_rake_task
+
 end
