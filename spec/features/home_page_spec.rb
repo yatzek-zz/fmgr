76dd34d65a771e0ac43176b5118ae9c5915be57d
Jacek Szlachta <jacek.szlachta@gmail.com>
2013-07-10 23:36:21 +0100

    show last game on the landing page

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -5,20 +5,18 @@ feature 'Home page' do
 
     scenario 'visit root url - no game yet' do
       visit root_url
-      page.should have_content 'No game yet'
+      page.should have_content 'No games yet'
     end
 
     scenario 'visit root url - next game found' do
 
-      _TIME_29_01_2013_09_15 = Time.local(2013, 1, 29, 9, 15, 0)
-      _TIME_29_01_2013_12_00 = Time.local(2013, 1, 29, 12, 0, 0)
+      time_29_01_2013_12_00 = Time.local(2013, 1, 29, 12, 0, 0)
 
-      Timecop.freeze(_TIME_29_01_2013_09_15)
-      @game = create(:game, time: _TIME_29_01_2013_12_00)
+      @game = create(:game, time: time_29_01_2013_12_00)
 
       visit root_url
 
-      page.should have_content 'Next game on Tuesday 29-Jan-2013 12:00'
+      page.should have_content 'Game on Tuesday 29-Jan-2013 12:00'
       page.should have_content 'Players'
     end
 
