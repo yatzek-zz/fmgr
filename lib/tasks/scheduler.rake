e8977df0fa140e33563093dd53d93d5bf1e9afe2
Jacek Szlachta <jacek.szlachta@gmail.com>
2013-07-10 23:18:44 +0100

    tidy-up

--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -1,21 +1,21 @@
-require "heroku-api"
+require 'heroku-api'
 
 API_KEY = 'ff168fd3606f0727348d73a5a843272c523459aa'
 APP_NAME = 'fmgr'
 
 # to run: heroku run rake scale[0]
-desc "Scale dynos"
+desc 'Scale dynos'
 task :scale, :instances do |t, args|
   heroku = Heroku::API.new api_key: API_KEY
   heroku.post_ps_scale(APP_NAME, 'web', args[:instances])
 end
 
-desc "create games periodic task"
+desc 'create games periodic task'
 task :create_games => :environment do
   GameDefinition.create_games
 end
 
-desc "send notification emails task"
+desc 'send notification emails task'
 task :send_notification_emails => :environment do
   Game.send_notification_emails
 end
--- spec/models/game_definition_spec.rb
+++ spec/models/game_definition_spec.rb
@@ -1,26 +1,26 @@
 require 'spec_helper'
-require "timecop"
+require 'timecop'
 
-describe "GameDefinition" do
+describe 'GameDefinition' do
 
-  it "has a valid factory" do
+  it 'has a valid factory' do
     create(:game_definition).should be_valid
   end
 
-  it "is invalid without a day" do
+  it 'is invalid without a day' do
     build(:game_definition, day: nil).should_not be_valid
   end
 
-  it "is invalid without a time" do
+  it 'is invalid without a time' do
     build(:game_definition, time: nil).should_not be_valid
   end
 
-  it "is unique for date and time" do
+  it 'is unique for date and time' do
     create(:game_definition)
     build(:game_definition).should_not be_valid
   end
 
-  describe "has periodic job which:" do
+  describe 'has periodic job which:' do
 
     before :each do
       @tue_12_00_game = create :game_definition
@@ -30,14 +30,14 @@ describe "GameDefinition" do
     TIME_10_01_2013_13_00 = Time.local(2013, 1, 10, 13, 0, 0)
     TIME_17_01_2013_13_00 = Time.local(2013, 1, 17, 13, 0, 0)
 
-    it "does not create next game instance if not withing creation period" do
+    it 'does not create next game instance if not withing creation period' do
       Timecop.freeze TIME_10_01_2013_11_00 # more than 5 days before the game, next game: 15.01.2013 12:00
       GameDefinition.create_games
 
       Game.all.should be_empty
     end
 
-    it "creates next game instance if within creation period and none exists" do
+    it 'creates next game instance if within creation period and none exists' do
       Timecop.freeze TIME_10_01_2013_13_00 # less than 5 days before the game, next game: 15.01.2013 12:00
       GameDefinition.create_games
 
@@ -47,7 +47,7 @@ describe "GameDefinition" do
       game.time.should == @tue_12_00_game.next_game_time
     end
 
-    it "does not create next game instance if within creation period and game instance exists" do
+    it 'does not create next game instance if within creation period and game instance exists' do
       Timecop.freeze TIME_10_01_2013_13_00 # less than 5 days before the game, next game: 15.01.2013 12:00
       create(:game, game_definition: @tue_12_00_game, time: @tue_12_00_game.next_game_time)
 
@@ -58,7 +58,7 @@ describe "GameDefinition" do
       Game.all.size.should == 1
     end
 
-    it "creates next game instance if within creation period and past week game instance exists" do
+    it 'creates next game instance if within creation period and past week game instance exists' do
       Timecop.freeze TIME_10_01_2013_13_00 # less than 5 days before the game, next game: 15.01.2013 12:00
       first_game_time = @tue_12_00_game.next_game_time
       create(:game, game_definition: @tue_12_00_game, time: first_game_time)
