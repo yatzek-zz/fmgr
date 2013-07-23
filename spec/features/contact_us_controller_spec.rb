a47a931b6233935cd9d863e031df3743b7cdd0eb
Jacek Szlachta <jacek.szlachta@gmail.com>
2013-07-24 00:02:18 +0100

    TODO file, tidy-up

--- spec/features/contact_us_controller_spec.rb
+++ spec/features/contact_us_controller_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
-feature "ContactUsController" do
+feature 'ContactUsController' do
 
-  scenario "renders Contact Us form" do
+  scenario 'renders Contact Us form' do
     visit contact_us_path
     page.should have_content 'Name'
     page.should have_content 'Email'
@@ -10,7 +10,7 @@ feature "ContactUsController" do
     page.should have_button 'Submit'
   end
 
-  scenario "redirects to the root path when submitted successfully" do
+  scenario 'redirects to the root path when submitted successfully' do
     visit contact_us_path
     fill_in 'Name', :with => 'John Rambo'
     fill_in 'Email', :with => 'rambo@rambo.com'
@@ -19,7 +19,7 @@ feature "ContactUsController" do
     current_path.should == root_path
   end
 
-  scenario "shows validation errors when invalid" do
+  scenario 'shows validation errors when invalid' do
     visit contact_us_path
     click_button 'Submit'
     current_path.should == send_mail_path
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -14,19 +14,19 @@ require 'spec_helper'
 
 describe 'Game' do
 
-  it "has a valid factory" do
+  it 'has a valid factory' do
     create(:game).should be_valid
   end
 
-  it "is invalid without a time" do
+  it 'is invalid without a time' do
     build(:game, time: nil).should_not be_valid
   end
 
-  it "is invalid without a game_definition" do
+  it 'is invalid without a game_definition' do
     build(:game, game_definition: nil).should_not be_valid
   end
 
-  it "is unique for game_definition and time" do
+  it 'is unique for game_definition and time' do
     game_definition = create(:game_definition)
     time = game_definition.next_game_time
 
@@ -34,14 +34,14 @@ describe 'Game' do
     build(:game, game_definition: game_definition, time: time).should_not be_valid
   end
 
-  describe "has periodic job which" do
+  describe 'has periodic job which' do
 
-    it "does not send emails to players if there are no game instances" do
+    it 'does not send emails to players if there are no game instances' do
       Game.send_notification_emails
       email_deliveries.should be_empty
     end
 
-    it "does not send emails to players for an old game instance" do
+    it 'does not send emails to players for an old game instance' do
       game_definition = create(:game_definition)
       create(:game, game_definition: game_definition,
                     time: game_definition.next_game_time,
@@ -53,7 +53,7 @@ describe 'Game' do
       email_deliveries.should be_empty
     end
 
-    it "sends emails to players for a new game instance" do
+    it 'sends emails to players for a new game instance' do
       game_definition = create(:game_definition)
       game = create(:game, game_definition: game_definition, time: game_definition.next_game_time)
       szlachta = create(:szlachta)
