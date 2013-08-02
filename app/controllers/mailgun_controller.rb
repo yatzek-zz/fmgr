a8e1869c836f226eaeb31ab191d4c072c0a292b7
Jacek Szlachta <jacek.szlachta@gmail.com>
2013-08-02 23:38:37 +0100

    mailgun hook controller added

--- app/controllers/mailgun_controller.rb
+++ app/controllers/mailgun_controller.rb
@@ -1,7 +1,7 @@
 class MailgunController < ApplicationController
 
   def failed_hook
-    MailgunHookMailer.not_delivered(params)
+    MailgunHookMailer.not_delivered(params).deliver
     logger.error "Mailgun failed delivery error, params: #{params.inspect}"
     render inline: 'OK'
   end
