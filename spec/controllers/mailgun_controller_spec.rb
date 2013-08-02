ddc74667bf3d0189b010ccb8f8d0ca964d336733
Jacek Szlachta <jacek.szlachta@gmail.com>
2013-08-02 23:38:37 +0100

    mailgun hook controller added

--- spec/controllers/mailgun_controller_spec.rb
+++ spec/controllers/mailgun_controller_spec.rb
@@ -6,6 +6,7 @@ describe MailgunController do
     it 'returns http success' do
       post 'failed_hook'
       response.should be_success
+      last_email.to.should include 'jacek.szlachta@gmail.com'
     end
   end
 
