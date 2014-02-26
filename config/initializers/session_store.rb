7b6749402da7f711390f64de24fc91bf5f8a4522
mysky <dl-cgmysky@bskyb.com>
2014-02-26 22:34:56 +0000

    re-enable sessions - broken flash messages

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,4 +3,4 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-Fmgr::Application.config.session_store :disabled
+#Fmgr::Application.config.session_store :disabled
